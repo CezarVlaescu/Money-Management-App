36dfbea06eec6ddcc043d170168e99377e2e261d
Vlaescu Cezar <vlaescucezar@yahoo.com>
2026-03-11 17:20:38 +0200

    update 11.03

---
+++ src/app/pages/dashboard-setup/dashboard-setup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-setup',
+  imports: [],
+  templateUrl: './dashboard-setup.component.html',
+  styleUrl: './dashboard-setup.component.scss'
+})
+export class DashboardSetupComponent {
+
+}
