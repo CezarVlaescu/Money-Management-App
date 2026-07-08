03230f64f2dc430e2f7616e328c0a25a96565d32
Vlaescu Cezar <vlaescucezar@yahoo.com>
2026-07-08 14:54:06 +0300

    update 08/07 15:00

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://anxcxtdvzzfpgnenrynh.supabase.co',
-  supabasePublishableKey: 'sb_publishable_GZKNKszeTgW4qJJhGS88Kg_pxD_u4cF'
+  supabaseUrl: '',
+  supabasePublishableKey: ''
 };
